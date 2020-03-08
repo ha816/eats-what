671ca13cfb6c5bfea255139af94e5c4f9bee42b6
이승민 <haha816@tmon.co.kr>
2020-03-08 18:57:13 +0900

    [2020-03-28]eats-what - SpringBoot 기반 기초 세팅 배포

---
+++ src/main/java/example/app/Application.java
@@ -0,0 +1,22 @@
+package example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
